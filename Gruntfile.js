8fce42661bbd659fc38a566a1d15c9f7085c0686
samuel <samuelsilvawb@gmail.com>
2016-12-31 18:41:54 -0300

    registrando novas tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,23 @@
- 
+module.exports = function(grunt){
+    
+    grunt.initConfig({
+        copy:{
+            main:{
+                expand:true,
+                src:"**",
+                dest:"dist"
+            }
+        },
+        clean:{
+            dist:{
+                src:"dist"
+            }
+        }
+    });
+    
+    grunt.registerTask("dist",["clean","copy"]);
+    grunt.registerTask("default",["dist"]);
+    
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+} 
