bc917d47dc3d24d97c7cd798b72810ed4e098f9c
samuel silva <samuelsilvawb@gmail.com>
2017-01-12 16:45:49 -0300

    preenchendo objeto corretamentet

--- js/newPage.js
+++ js/newPage.js
@@ -9,24 +9,23 @@ document.getElementById("btn-frm-contact-2").addEventListener("click",function(e
 });
 
 var biografia = {
-    name:"Samuel Silva",
-    role:"Web Developer",
-    idade:20,
+    name:"",
+    role:"",
+    idade:,
     habilidades:["HTML5", "CSS3", "javascript", "jQuery", "less", "Grunt", "Gulp", "Java", "PHP"],
     contacts:{
-        "mobile":"(85)99230-9769",
-        "email":"samuelsilvawb@gmail.com",
-        "github":"samuelsilvadev",
-        "twitter":"@samuelsilvadev",
-        "location":"Fortaleza - CE"
+        "mobile":"",
+        "email":"",
+        "github":"",
+        "twitter":"",
+        "location":""
     },
     Biografia:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
     imagemDoPerfil:"images/fry.jpg"
 };
 
 function getFormPerson(){
-	console.log(document.getElementById("frm-initial").elements[0]);
-	for (let i = 0; i <= document.getElementById("frm-initial").elements.length; i++) {
+	for (let i = 0; i < document.getElementById("frm-initial").elements.length; i++) {
 		
 		let name = document.getElementById("frm-initial").elements[i].name;
 		let value = document.getElementById("frm-initial").elements[i].value;
@@ -41,9 +40,10 @@ function getFormPerson(){
 }
 
 function getFormContact(){
-	for (let i = 0; i <= document.getElementById("frm-contacts").elements.length; i++) {
-		let name = document.getElementById("frm-contacts").elements[i+1].name;
-		let value = document.getElementById("frm-contacts").elements[i+1].value;
+	for (let i = 0; i < document.getElementById("frm-contacts").elements.length; i++) {
+		
+		let name = document.getElementById("frm-contacts").elements[i].name;
+		let value = document.getElementById("frm-contacts").elements[i].value;
 		
 		if(name === "mobile"){
 			biografia.contacts.mobile = value;
